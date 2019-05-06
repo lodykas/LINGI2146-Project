227813ae68d1ae91afa9019cd2e9291a28afb3d4
Instant Contiki <user@instant-contiki.(none)>
2019-05-06 17:26:33 +0200

    packet_queue, and new recv architecture

--- node/node.c
+++ node/node.c
@@ -1,8 +1,8 @@
 /**
  * \file
- *         Testing the broadcast layer in Rime
+ *		 Testing the broadcast layer in Rime
  * \author
- *         Adam Dunkels <adam@sics.se>
+ *		 Adam Dunkels <adam@sics.se>
  */
 
 #include "contiki.h"
@@ -14,62 +14,147 @@
 #include "dev/leds.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+//messages
+#define HELLO 0
+#define WELCOME 1
+
 /*---------------------------------------------------------------------------*/
-PROCESS(example_broadcast_process, "Broadcast example");
+PROCESS(example_broadcast_process, "Routing tree discovery");
 AUTOSTART_PROCESSES(&example_broadcast_process);
 
-char *hello = "Hello";
-char *groot = "I'm (g)root";
-char *welcome = "Welcome";
+struct discovery_struct {
+	uint8_t msg;
+	uint8_t weigth;
+} ;
+typedef struct discovery_struct discovery_struct_t;
+
+union discovery_union {
+	char* c;
+	discovery_struct_t* st;
+};
+typedef union discovery_union discovery_t;
+
+
+
+struct queue_elem {
+	discovery_t* data;
+	struct queue_elem* next;
+};
+struct queue{
+	struct queue_elem* head;
+	struct queue_elem* tail;
+};
+typedef struct queue queue_t;
+
+void queue_add(queue_t* queue, discovery_t* data){
+	struct queue_elem* elem = (struct queue_elem*) malloc(sizeof(struct queue_elem));
+	if(elem == 0){
+		printf("error assigning memory");
+		exit(1);
+	}
+	
+	elem->data = data;
+	elem->next = 0;
+	
+	queue->tail->next = elem;
+	queue->tail = elem;
+	
+	if(queue->head == 0){
+		queue->head = elem;
+	}
+}
+discovery_t* queue_pop(queue_t* queue){
+
+	discovery_t* data = queue->head->data;
+	struct queue_elem* old_head = queue->head;
+	if(queue->head == queue->tail){
+		queue->tail = 0;
+	}
+	
+	if(queue->head->next != 0){
+		queue->head = queue->head->next;
+	}
+	else{
+		queue->head = 0;
+	}
+	free(old_head);
+	return data;
+}
+
+
+
+uint8_t weight = 255; 
+static struct broadcast_conn broadcast;
+queue_t packet_queue;
+
+
 /*---------------------------------------------------------------------------*/
+
 static void broadcast_recv(struct broadcast_conn *c, const rimeaddr_t *from)
 {
-    char *message = (char *)packetbuf_dataptr();
-    printf("broadcast message received from %d.%d: '%s'\n",
-         from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
-    
-    if (strncmp(groot, message, 12) == 0)
-    {
-        printf("Root detected with address %d.%d\n", from->u8[0], from->u8[1]);
-    }
-    else if (strncmp(hello, message, 6) == 0) 
-    {
-        printf("New node detected : %d.%d\n", from->u8[0], from->u8[1]);
-    }
-    else if (strncmp(welcome, message, 8) == 0)
-    {
-        printf("Welcome received from %d.%d\n", from->u8[0], from->u8[1]);
-    }
+	discovery_t message;
+	message.c = (char*) packetbuf_dataptr();
+	printf("MSG: %x\n",message.st->msg);
+		 
+	discovery_t response;
+	switch(message.st->msg){
+		case HELLO:
+			printf("HELLO message received from %d.%d: '%d'\n",from->u8[0], from->u8[1], message.st->msg);
+			response.st->msg = WELCOME;
+			response.st->weigth = weight;
+			break;
+		case WELCOME:
+			printf("WELCOME message received from %d.%d: '%d'\n",from->u8[0], from->u8[1], message.st->msg);
+			break;
+		default:
+			break;
+		
+	}
+	queue_add(&packet_queue,&response);
 }
 static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
-static struct broadcast_conn broadcast;
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(example_broadcast_process, ev, data)
 {
-    char *hello = "Hello";
-    static struct etimer et;
+	static struct etimer et;
 
-    PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
+	PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
 
-    PROCESS_BEGIN();
+	PROCESS_BEGIN();
 
-    broadcast_open(&broadcast, 129, &broadcast_call);
-    
-    /* Send hello */
-    packetbuf_copyfrom(hello, 6);
-    broadcast_send(&broadcast);
+	broadcast_open(&broadcast, 129, &broadcast_call);
+	
+	/* Send hello */
+	discovery_t msg;
+	msg.st->msg = HELLO;
+	msg.st->weigth = 0;
+	
 
-    while(1) {
+	while(1) {
+	
+		printf("SENT: %x\n",msg.st->msg);
+		packetbuf_copyfrom(msg.c, sizeof(discovery_struct_t));
+		broadcast_send(&broadcast);
 
-        /* Delay 2-4 seconds */
-        etimer_set(&et, CLOCK_SECOND * 4 + random_rand() % (CLOCK_SECOND * 4));
+		/* Delay 2-4 seconds */
+		etimer_set(&et, CLOCK_SECOND * 4 + random_rand() % (CLOCK_SECOND * 4));
 
-        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
-    
-    
-    }
+		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+		
+		while(packet_queue.head != 0){
+			discovery_t* to_send;
+			to_send = queue_pop(&packet_queue);
+			packetbuf_copyfrom(to_send->c, sizeof(discovery_struct_t));
+			broadcast_send(&broadcast);
+			free(to_send);
+		}
+	
+	
+	}
 
-    PROCESS_END();
+	PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
