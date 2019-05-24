44df6ce50febe322d0264266a48f0aab7c77069d
Instant Contiki <user@instant-contiki.(none)>
2019-05-24 12:16:36 +0200

    new subscribe mechanic, test client

---
+++ clientmqtt.py
@@ -0,0 +1,19 @@
+import paho.mqtt.client as mqtt
+broker = "127.0.0.1"
+port = 1234
+channel_prefix = "ME_34_DATA"
+client = mqtt.Client("client1")
+
+def on_message_callback(client, userdata, message):
+   print(str(message.payload.decode("utf-8")))
+
+client.connect(broker, port=port, keepalive=60, bind_address="")
+client.on_message = on_message_callback
+client.connect(broker, port=port, keepalive=60, bind_address="")
+client.subscribe(channel_prefix+"/5.0/temperature")
+client.subscribe(channel_prefix+"/6.0/temperature")
+client.subscribe(channel_prefix+"/7.0/temperature")
+client.subscribe(channel_prefix+"/8.0/temperature")
+client.subscribe(channel_prefix+"/9.0/temperature")
+
+client.loop_forever()
--- gateway.py
+++ gateway.py
@@ -5,10 +5,17 @@ import paho.mqtt.client as mqtt
 import subprocess
 import sys
 
-broker = "test.mosquitto.org"
-port = 1883
+broker = "127.0.0.1"
+port = 1234
 serialDevice = "/dev/pts/7"
+channel_prefix = "ME_34_DATA"
 
+subscribers = {}
+
+p = subprocess.Popen(["/home/user/contiki/tools/sky/serialdump-linux", "-b115200", serialDevice], stdout=subprocess.PIPE, stdin=subprocess.PIPE, bufsize=1, universal_newlines=True)
+
+
+#This thread class is in charge of the command-line interface
 class CommandTool (threading.Thread):
 	def __init__(self, threadID, file):
 		threading.Thread.__init__(self)
@@ -24,13 +31,51 @@ class CommandTool (threading.Thread):
 				break
 			self.file.stdin.write(line)
 			
-
+#subscribe to all topics of interest whenever we connect to broker
 def on_connect_callback(client, userdata, flags, rc):
 	print("Connected with result code "+str(rc))
-	#subsribe here
+	client.subscribe("$SYS/broker/log/M/subscribe")
+	client.subscribe("$SYS/broker/log/M/unsubscribe")
+	client.subscribe(channel_prefix+"subscription/#")
 
-def on_message_callback():
-	pass
+#here we process the arrival/departure of subscribers to limit network usage
+def on_message_callback(client, userdata, message):
+	print(" msg : "+str(message.payload.decode("utf-8")))
+	msg = str(message.payload.decode("utf-8")).split()
+	if message.topic == "$SYS/broker/log/M/subscribe":
+		channel = msg[3]
+		if channel in subscribers:
+			subscribers[channel]+=1
+		else:
+			subscribers[channel]=1
+			parts = channel.split('/')
+			p.stdin.write("DATA-PER "+ parts[0])
+	elif message.topic == "$SYS/broker/log/M/unsubscribe":
+		channel = msg[3]
+		if channel in subscribers:
+			subscribers[channel]-=1
+			if (subscribers[channel]) == 0:
+				del subscribers[channel]
+				parts = channel.split('/')
+				p.stdin.write("DATA-NEV "+ parts[0])
+
+	#this is an active way of joining/leaving
+	elif channel_prefix+"subscription/" in message.topic:
+		channel = message.topic[len( channel_prefix+"subscription/"):]
+		if(msg[0] == "subscribe"):
+			if channel in subscribers:
+				subscribers[channel]+=1
+			else:
+				subscribers[channel]=1
+			parts = channel.split('/')
+			p.stdin.write("DATA-PER "+ parts[0])
+		elif(msg[0] == "unsubscribe"):
+			if channel in subscribers:
+				subscribers[channel]-=1
+				if (subscribers[channel]) == 0:
+					del subscribers[channel]
+					parts = channel.split('/')
+					p.stdin.write("DATA-NEV "+ parts[0])
 
 def main():
 	client = mqtt.Client("Gateway")
@@ -38,9 +83,6 @@ def main():
 	client.on_message = on_message_callback
 
 	client.connect(broker, port=port, keepalive=60, bind_address="")
-
-
-	p = subprocess.Popen(["/home/user/contiki/tools/sky/serialdump-linux", "-b115200", serialDevice], stdout=subprocess.PIPE, stdin=subprocess.PIPE, bufsize=1, universal_newlines=True)
 
 	ct = CommandTool(0,p)
 	ct.start()
@@ -51,8 +93,8 @@ def main():
 		line = p.stdout.readline()
  		data = line.split()
 		if (len(data) == 2):
-			#print("data received on topic : "+ data[0])
-			client.publish("ME_34_DATA/"+data[0],data[1])
+			client.publish(channel_prefix+data[0],data[1])
+			print(channel_prefix+data[0]+" - "+data[1])
 		else:
 			print "response: "+line
 
